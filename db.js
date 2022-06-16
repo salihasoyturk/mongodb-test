e2ec0753422a9fc01b56af03f08ee62f72acf358
salihasoyturk <soyturk.saliha@gmail.com>
2022-06-16 11:29:55 +0300

    gitignore added

--- db.js
+++ db.js
@@ -1,18 +1,25 @@
 const mongoose = require("mongoose")
 
-const start = async () => {
-	if (!process.env.MONGO_URI) {
-		throw new Error("auth DB_URI must be defined")
-	}
-	try {
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		})
-		console.log("Server connected to MongoDb!")
-	} catch (err) {
-		console.error(err)
-	}
-}
+// const start = async () => {
+// 	if (!process.env.MONGO_URI) {
+// 		throw new Error("auth DB_URI must be defined")
+// 	}
+// 	try {
+// 		await mongoose.connect(process.env.MONGO_URI, {
+// 			useNewUrlParser: true,
+// 			useUnifiedTopology: true,
+// 		})
+// 		console.log("Server connected to MongoDb!")
+// 	} catch (err) {
+// 		console.error(err)
+// 	}
+// }
 
-module.exports = start
+// module.exports = start
+
+mongoose.connect('mongodb://localhost/testaroo');
+
+Element.on
+mongoose.connection.once('open', function () {
+
+})
