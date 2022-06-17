7e53b0afae22900efce6a53a198537e2355b3b13
salihasoyturk <soyturk.saliha@gmail.com>
2022-06-17 11:13:25 +0300

    mongodb'ye baglandi

--- db.js
+++ db.js
@@ -1,25 +1,18 @@
 const mongoose = require("mongoose")
 
-// const start = async () => {
-// 	if (!process.env.MONGO_URI) {
-// 		throw new Error("auth DB_URI must be defined")
-// 	}
-// 	try {
-// 		await mongoose.connect(process.env.MONGO_URI, {
-// 			useNewUrlParser: true,
-// 			useUnifiedTopology: true,
-// 		})
-// 		console.log("Server connected to MongoDb!")
-// 	} catch (err) {
-// 		console.error(err)
-// 	}
-// }
+const start = async () => {
+	if (!process.env.MONGO_URI) {
+		throw new Error("auth DB_URI must be defined")
+	}
+	try {
+		await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		console.log("Server connected to MongoDb!")
+	} catch (err) {
+		console.error(err)
+	}
+}
 
-// module.exports = start
-
-mongoose.connect('mongodb://localhost/testaroo');
-
-Element.on
-mongoose.connection.once('open', function () {
-
-})
+module.exports = start
